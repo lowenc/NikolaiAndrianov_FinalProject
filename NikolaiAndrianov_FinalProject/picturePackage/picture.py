a3419f05ea258010c3c7491ab7fe915d309e4e51
lowenc <lowenc@LCBLAB3120J-46.uccob.uc.edu>
2023-04-18 14:05:09 -0400

    Merge branch 'main' of https://github.com/lowenc/NikolaiAndrianov_FinalProject.git into main
    Added Flowerbox

--- NikolaiAndrianov_FinalProject/picturePackage/picture.py
+++ NikolaiAndrianov_FinalProject/picturePackage/picture.py
@@ -1,5 +1,10 @@
-'''
-Created on Apr 18, 2023
+#Name: Nikolai Andrianov - Nathan Lowe, Hugo Bourinbayar, Payton Cook, Jimmy Zimmer
+#email: lowenc@mail.uc.edu, bourinhh@mail.uc.edu, cookpk@mail.uc.edu, zimmejc@mail.uc.edu
+#Assignment Title: Final Project
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: 
+#Citations: 
+#Anything else that's relevant:
 
-@author: bourinhh
-'''
+# picture.py
