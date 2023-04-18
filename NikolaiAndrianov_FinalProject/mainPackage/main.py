12edc19eb40b37b893b3118d884408f4e3a5a5fd
bourinhh <bourinhh@LCBLAB3120J-42.uccob.uc.edu>
2023-04-18 14:03:30 -0400

    added documentation

--- NikolaiAndrianov_FinalProject/mainPackage/main.py
+++ NikolaiAndrianov_FinalProject/mainPackage/main.py
@@ -1,2 +1,8 @@
-# main.py
-#This is a test commit 2
+# Name: Hugo Bourinbayar, 
+# email: bourinhh@mail.uc.edu, 
+# Assignment Title: Final Project
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: This project demonstrates that I can use everything we learned in class
+# Citations: Bill Nicholson
+# Anything else that's relevant:
--- NikolaiAndrianov_FinalProject/picturePackage/picture.py
+++ NikolaiAndrianov_FinalProject/picturePackage/picture.py
@@ -1,5 +1,8 @@
-'''
-Created on Apr 18, 2023
-
-@author: bourinhh
-'''
+# Name: Hugo Bourinbayar, 
+# email: bourinhh@mail.uc.edu, 
+# Assignment Title: Final Project
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: This project demonstrates that I can use everything we learned in class
+# Citations: Bill Nicholson
+# Anything else that's relevant:
