8f0270555d898768aeda4903deef448efb977595
lowenc <lowenc@LCBLAB3120J-46.uccob.uc.edu>
2023-04-18 14:02:06 -0400

    Flower box added

--- NikolaiAndrianov_FinalProject/mainPackage/main.py
+++ NikolaiAndrianov_FinalProject/mainPackage/main.py
@@ -1,2 +1,10 @@
-# main.py
-#This is a test commit 2
+#Name: Nikolai Andrianov - Nathan Lowe, Hugo Bourinbayar, Payton Cook, Jimmy Zimmer
+#email: lowenc@mail.uc.edu, bourinhh@mail.uc.edu, cookpk@mail.uc.edu, zimmejc@mail.uc.edu
+#Assignment Title: Final Project
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: 
+#Citations: 
+#Anything else that's relevant:
+
+#main.py
